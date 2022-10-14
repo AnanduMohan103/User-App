a18179885d4c95d9c6c0e72e87097c40c5309b4c
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-14 17:21:38 +0530

    new

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myRouter:Router) { }
+userName=""
+password=""
 
+readValue=()=>{
+  let data={
+    "userName":this.userName,
+    "password":this.password
+  }
+  if (this.userName=="admin" && this.password=="12345") {
+    this.myRouter.navigate(["/dash"])
+    
+  } else {
+    alert("invalid credential")
+  }
+  console.log(data)
+
+  this.userName=""
+  this.password=""
+}
   ngOnInit(): void {
   }
 
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,6 +9,57 @@ export class SignupComponent implements OnInit {
 
   constructor() { }
 
+  name=""
+  phone=""
+  gender=""
+  email=""
+  address=""
+  pincode=""
+  bloodgroup=""
+  dob=""
+  age=""
+  parentname=""
+  username=""
+  password=""
+  confirmpassword=""
+
+
+  readValue=()=>{
+    let data={
+      "name":this.name,
+      "phone":this.phone,
+      "gender":this.gender,
+      "email":this.email,
+      "address":this.address,
+      "pincode":this.pincode,
+      "bloodgroup":this.bloodgroup,
+      "dob":this.dob,
+      "age":this.age,
+      "username":this.username,
+      "parentname":this.parentname,
+      "password":this.password,
+      "confirmpassword":this.confirmpassword
+    }
+    console.log(data)
+   
+    alert("successfully added")
+    this.name=""
+    this.phone=""
+    this.gender=""
+    this.email=""
+    this.address=""
+    this.pincode=""
+    this.bloodgroup=""
+    this.dob=""
+    this.age=""
+    this.username=""
+    this.parentname=""
+    this.password=""
+    this.confirmpassword=""
+
+  }
+  
+
   ngOnInit(): void {
   }
 
