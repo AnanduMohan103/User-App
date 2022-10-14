fabe4f717fb98c036b020c724655dfb7298b43e7
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-14 14:46:50 +0530

    sigin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashComponent } from './dash/dash.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashComponent
+    DashComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
